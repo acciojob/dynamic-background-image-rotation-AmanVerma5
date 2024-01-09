115b9461d103b686c34fd6379420ae6e4ca883c5
AmanVerma5 <118344057+AmanVerma5@users.noreply.github.com>
2024-01-09 14:10:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
-let imageContainer=document.getElementByClass('image-container')[0];
+
 if(window.innerWidth>window.innerHeight){
 	document.body.style.backgroundImage=`url('https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Paysage-en-orientation-paysage.jpg')`
 }else{
